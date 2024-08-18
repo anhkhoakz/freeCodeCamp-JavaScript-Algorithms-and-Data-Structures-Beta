03be0a0aa8e5c07ea0b683aebb562be70d1c54d4
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-08-18 18:51:22 +0700

    Add lazy loading for images and update project list in index.html

--- Learn Introductory JavaScript by Building a Pyramid Generator/script.js
+++ Learn Introductory JavaScript by Building a Pyramid Generator/script.js
@@ -4,21 +4,25 @@ const rows = [];
 let inverted = true;
 
 function padRow(rowNumber, rowCount) {
-  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+    return (
+        " ".repeat(rowCount - rowNumber) +
+        character.repeat(2 * rowNumber - 1) +
+        " ".repeat(rowCount - rowNumber)
+    );
 }
 
 for (let i = 1; i <= count; i++) {
-  if (inverted) {
-    rows.unshift(padRow(i, count));
-  } else {
-    rows.push(padRow(i, count));
-  }
+    if (inverted) {
+        rows.unshift(padRow(i, count));
+    } else {
+        rows.push(padRow(i, count));
+    }
 }
 
-let result = ""
+let result = "";
 
 for (const row of rows) {
-  result = result + "\n" + row;
+    result = result + "\n" + row;
 }
 
 console.log(result);
